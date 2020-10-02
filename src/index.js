86a094ffede742e9fe006b6fb785adb01823224d
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-10-02 05:39:13 +0500

    adding bootstrap install in react

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import '..node_modules/bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
 
